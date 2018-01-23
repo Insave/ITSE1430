ace591aa93c72874e7081b51967f060f89f22556
PFief <patrick.fief@my.tccd.edu>
2018-01-22 21:37:47 -0600

    Added comments

--- Classwork/HelloWorld/HelloWorld/Program.cs
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -6,8 +6,12 @@ using System.Threading.Tasks;
 
 namespace HelloWorld
 {
+    // Single line comment
     class Program
     {
+        /*
+         * As many lines as I want to put in
+         */
         static void Main( string[] args )
         {
             Console.WriteLine("Hello World");
