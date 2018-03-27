db07aa98f36d84d32635c6b9c5d5eaeba4cb6aa8
PFief <patrick.fief@my.tccd.edu>
2018-03-26 21:35:45 -0500

    IO database implementation

---
+++ Classwork/Section4/Nile.Data.Memory/MemoryProductDatabase.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nile.Data.Memory
+{
+    /// <summary>Provides an in-memory product database.</summary>
+    public class MemoryProductDatabase : ProductDatabase
+    {
+        //public MemoryProductDatabase()
+        //{
+        //    //_products = new Product[25];
+        //    //_products = new List<Product>()
+        //    //{
+        //    //    new Product() 
+        //    //    {
+        //    //        Id = _nextID++,
+        //    //        Name = "iPhone X",
+        //    //        IsDiscontinued = true,
+        //    //        Price = 1500,
+        //    //    },
+        //    //    new Product() {
+        //    //        Id = _nextID++,
+        //    //        Name = "Windows Phone",
+        //    //        IsDiscontinued = true,
+        //    //        Price = 15,
+        //    //    },
+        //    //    new Product() {
+        //    //        Id = _nextID++,
+        //    //        Name = "Samsung S8",
+        //    //        IsDiscontinued = false,
+        //    //        Price = 800,
+        //    //    },
+        //    //};
+        //    this.Seed();
+        //}
+
+        protected override Product AddCore ( Product product )
+        {
+            //Clone the object
+            product.Id = _nextID++;
+            _products.Add(Clone(product));
+
+            //Return a copy
+            return product;
+        }
+
+        protected override Product UpdateCore ( Product product )
+        {
+            var existing = GetCore(product.Id);
+
+            // Clone the object
+            //_products[existingIndex] = Clone(product);
+            Copy(existing, product);
+
+            //Return a copy
+            return product;
+        }
+
+        protected override Product GetCore( int id )
+        {
+            foreach (var product in _products)
+            {
+                if (product.Id == id)
+                    return product;
+            };
+
+            return null;
+        }
+
+        protected override IEnumerable<Product> GetAllCore()
+        {
+            foreach (var product in _products)
+            {
+                if (product != null)
+                    yield return Clone(product);
+            }
+        }
+
+        protected override void RemoveCore ( int id )
+        {
+            var existing = GetCore(id);
+            if (existing != null)
+                _products.Remove(existing);
+        }
+
+        private Product Clone(Product item)
+        {
+            var newProduct = new Product();
+            Copy(newProduct, item);
+
+            return newProduct;
+        }
+
+        private void Copy(Product target, Product source)
+        {
+            target.Id = source.Id;
+            target.Name = source.Name;
+            target.Description = source.Description;
+            target.Price = source.Price;
+            target.IsDiscontinued = source.IsDiscontinued;
+        }
+
+        protected override Product GetProductByNameCore (string name)
+        {
+            foreach (var product in _products)
+            {
+                if (String.Compare(product.Name, name, true) == 0)
+                    return product;
+            };
+
+            return null;
+        }
+
+        private readonly List<Product> _products = new List<Product>();
+        private int _nextID = 1;
+    }
+}
---
+++ Classwork/Section4/Nile/Data/ProductDatabase.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nile.Data
+{
+    /// <summary>Provides an abstract product database.</summary>
+    public abstract class ProductDatabase : IProductDatabase
+    {
+        public Product Add ( Product product, out string message )
+        {
+            //Check for null
+            if (product == null)
+            {
+                message = "Product cannot be null.";
+                return null;
+            };
+
+            //Validate product
+            //var errors = ObjectValidator.Validate(product);
+            var errors = product.Validate();
+
+            //if (errors.Count() > 0)
+            //{
+            //    var error = Enumerable.FirstOrDefault(errors);
+            //    message = errors.ElementAt(0).ErrorMessage;
+            //    return null;
+            //};
+
+            var error = errors.FirstOrDefault();
+            if(error != null)
+            {
+                message = error.ErrorMessage;
+                return null;
+            }
+
+            // Verify unique product
+            var existing = GetProductByNameCore(product.Name);
+            if(existing != null)
+            {
+                message = "Product already exists.";
+                return null;
+            }
+
+            message = null;
+            return AddCore(product);
+        }
+
+        public Product Update ( Product product, out string message )
+        {
+            message = "";
+
+            //Check for null
+            if (product == null)
+            {
+                message = "Product cannot be null.";
+                return null;
+            };
+
+            //Validate product
+            var errors = product.Validate();
+            if (errors.Count() > 0)
+            {
+                message = errors.ElementAt(0).ErrorMessage;
+                return null;
+            };
+
+            // Verify unique product
+            var existing = GetProductByNameCore(product.Name);
+            if (existing != null && existing.Id != product.Id)
+            {
+                message = "Product already exists.";
+                return null;
+            }
+
+            //Find existing
+            existing = existing ?? GetCore(product.Id);
+            if (existing == null)
+            {
+                message = "Product not found.";
+                return null;
+            };
+            
+            return UpdateCore(product);
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return GetAllCore();
+        }
+
+        public void Remove ( int id )
+        {
+            if (id > 0)
+            {
+                RemoveCore(id);
+            };
+        }
+
+        protected abstract Product AddCore( Product product );
+        protected abstract IEnumerable<Product> GetAllCore();
+        protected abstract Product GetCore( int id );
+        protected abstract void RemoveCore( int id );
+        protected abstract Product UpdateCore( Product product );
+        protected abstract Product GetProductByNameCore( string name );
+        
+    }
+}
